1370d76d36e8ace649576914e1b7d198c8747da9
Jeffrey Sun <jeffrey.zksun@gmail.com>
2013-07-03 13:17:51 +0800

    Add more comments

--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
-The minimal package for mocha prototype
+ Tutorial: How to consume the cocha test framework.
+ 1. Create the test files under folder test/ and implement the test functions. All the js files under the test/ folder will be executed.
+ 2. Configure the package.json file to make the test run with the npm command.
+	a. Add the devDependencies. "devDependencies": {"mocha": "1.12.x","should": "1.2.x"}
+	b. Call the mocha command in the test scripts. "scripts": { "test": ".\\node_modules\\.bin\\mocha --require should --ui bdd --reporter list"}
--- test/application.js
+++ test/application.js
@@ -11,6 +11,11 @@ or
 	Source code: https://github.com/visionmedia/should.js
  - The tutorial how to consume mocha is here http://brianstoner.com/blog/testing-in-nodejs-with-mocha/
 
+ Tutorial: How to consume the cocha test framework.
+ 1. Create the test files under folder test/ and implement the test functions. All the js files under the test/ folder will be executed.
+ 2. Configure the package.json file to make the test run with the npm command.
+	a. Add the devDependencies. "devDependencies": {"mocha": "1.12.x","should": "1.2.x"}
+	b. Call the mocha command in the test scripts. "scripts": { "test": ".\\node_modules\\.bin\\mocha --require should --ui bdd --reporter list"}
 */
 var app = require("../");
 
